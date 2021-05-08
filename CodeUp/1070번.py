cdb6690d606ee1cba5c36856f41d458049dcf6c7
Inho <48787629+inho0919@users.noreply.github.com>
2021-05-09 07:37:40 +0900

    Create 1070번.py

---
+++ CodeUp/1070번.py
@@ -0,0 +1,9 @@
+season = int(input())
+if season == 12 or season == 1 or season == 2:
+    print("winter")
+elif season == 3 or season == 4 or season == 5:
+    print("spring")
+elif season == 6 or season == 7 or season == 8:
+    print("summer")
+else:
+    print("fall") 
