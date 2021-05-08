9b8579dd7327898a2c991565d116f653e0264c23
Inho <48787629+inho0919@users.noreply.github.com>
2021-05-09 07:27:11 +0900

    Create 1065번.py

---
+++ CodeUp/1065번.py
@@ -0,0 +1,13 @@
+a = input()
+list = a.split(" ")
+
+num1=int(list[0])
+num2=int(list[1])
+num3=int(list[2])
+
+if num1%2 == 0:
+    print(num1)
+if num2%2 == 0:
+    print(num2)
+if num3%2 == 0:
+    print(num3)
