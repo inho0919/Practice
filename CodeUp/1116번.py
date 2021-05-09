b2a18e8ad33fc42e45518764213341234ed9d8b1
Inho <48787629+inho0919@users.noreply.github.com>
2021-05-10 02:48:18 +0900

    Create 1116번.py

---
+++ CodeUp/1116번.py
@@ -0,0 +1,14 @@
+line = input()
+a = line.split(" ")
+a1 = int(a[0])
+a2 = int(a[1])
+mul = a1%a2
+plus = a1+a2
+minus = a1-a2
+ex = a1*a2
+div = int(a1/a2)
+
+print(a[0] + "+" + a[1] + "=" + str(plus))
+print(a[0] + "-" + a[1] + "=" + str(minus))
+print(a[0] + "*" + a[1] + "=" + str(ex))
+print(a[0] + "/" + a[1] + "=" + str(div))
