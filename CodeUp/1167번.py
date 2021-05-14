c18b3710725b4e98d78bd9ee20a694c0c6afd543
Inho <48787629+inho0919@users.noreply.github.com>
2021-05-14 09:04:07 +0900

    Create 1167번.py

---
+++ CodeUp/1167번.py
@@ -0,0 +1,31 @@
+item = input()
+oldline = item.split(" ")
+
+num1 = int(oldline[0])
+num2 = int(oldline[1])
+num3 = int(oldline[2])
+
+if num1 >= num2 :
+    if num2>=num3:
+        print(num2)
+    else:
+        if num1 >= num3:
+            print(num3)
+        else :
+            print(num1)
+elif num2 >= num3:
+    if num3>= num1 :
+        print(num3)
+    else :
+        if num1 >= num2:
+            print(num2)
+        else :
+            print(num1)
+elif num3>= num1:
+    if num1 >= num2:
+        print(num1)
+    else:
+        if num2 >= num3:
+            print(num3)
+        else :
+            print(num2)
