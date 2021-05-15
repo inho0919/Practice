43240b65e546b4b4d61a10d94b8c2baa05b1f174
Inho <48787629+inho0919@users.noreply.github.com>
2021-05-15 13:45:01 +0900

    Create 1202번.py

---
+++ CodeUp/1202번.py
@@ -0,0 +1,11 @@
+item = int(input())
+if item >= 90:
+    print("A")
+elif item>=80 and item<90:
+    print("B")
+elif item>=70 and item<80:
+    print("C")
+elif item>=60 and item<70:
+    print("D")
+else:
+    print("F")
