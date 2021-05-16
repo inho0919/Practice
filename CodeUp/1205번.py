0d7dec7b2934b484fd8197e319f7e1d844c616e8
Inho <48787629+inho0919@users.noreply.github.com>
2021-05-16 19:10:13 +0900

    Update 1205번.py

--- CodeUp/1205번.py
+++ CodeUp/1205번.py
@@ -4,28 +4,34 @@ new = item.split(" ")
 one = int(new[0])
 two = int(new[1])
 
-temp1 = one + two
-temp2 = one * two
-temp3 = one
-temp4 = two
-temp5 = one - two
-temp6 = float(one / two)
+temp1 = float(one + two)
+temp2 = float(two + one)
+temp3 = float(one - two)
+temp4 = float(two - one)
+temp5 = float(one * two)
+temp6 = float(two * one)
+temp7 = float(one / two)
+temp8 = float(two / one)
+temp9 = float(one ** two)
+temp10 = float(two ** one)
 
-for i in range(1, two):
-    temp3 = temp3 * one
+a = list()
+a.append(temp1)
+a.append(temp2)
+a.append(temp3)
+a.append(temp4)
+a.append(temp5)
+a.append(temp6)
+a.append(temp7)
+a.append(temp8)
+a.append(temp9)
+a.append(temp10)
 
-for i in range(1, one):
-    temp4 = temp4 * two
+for i in range(0, len(a)):
+    for j in range(0, len(a)):
+        if a[i] > a[j]:
+            temp = a[i]
+            a[i] = a[j]
+            a[j] = temp
 
-if temp1 >= temp2 and temp1 >= temp3 and temp1 >= temp4 and temp1>=temp5 and temp1>=temp6:
-    print("%.6f" % temp1)
-elif temp2 >= temp1 and temp2 >= temp3 and temp2 >= temp4 and temp2>=temp5 and temp2>=temp6:
-    print("%.6f" % temp2)
-elif temp3 >= temp1 and temp3 >= temp2 and temp3 >= temp4 and temp3>=temp5 and temp3>=temp6:
-    print("%.6f" % temp3)
-elif temp4 >= temp1 and temp4 >= temp2 and temp4 >= temp3 and temp4>=temp5 and temp4>=temp6:
-    print("%.6f" % temp4)
-elif temp5 >= temp1 and temp5 >= temp2 and temp5 >= temp3 and temp5>=temp4 and temp4>=temp6:
-    print("%.6f" % temp5)
-elif temp6 >= temp1 and temp6 >= temp2 and temp6 >= temp3 and temp6>=temp4 and temp6>=temp5:
-    print("%.6f" % temp6)    
+print("%.6f" % a[0])
