7f6cbaf3a663c89f5ab1a98e7e5d55bb4321b0c2
Inho <48787629+inho0919@users.noreply.github.com>
2021-05-18 10:49:22 +0900

    Create 6065번.py

---
+++ CodeUp/6065번.py
@@ -0,0 +1,13 @@
+a = input()
+b = a.split(" ")
+
+num1 = int(b[0])
+num2 = int(b[1])
+num3 = int(b[2])
+
+if num1%2 == 0:
+    print(num1)
+if num2%2 == 0:
+    print(num2)
+if num3%2 == 0:
+    print(num3)
