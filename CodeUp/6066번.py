04626e04f73a2473c6b1fd443775c693bb0d4480
Inho <48787629+inho0919@users.noreply.github.com>
2021-05-18 10:50:11 +0900

    Create 6066번.py

---
+++ CodeUp/6066번.py
@@ -0,0 +1,19 @@
+a = input()
+b = a.split(" ")
+
+num1 = int(b[0])
+num2 = int(b[1])
+num3 = int(b[2])
+
+if num1%2 == 0:
+    print("even")
+elif num1%2 != 0:
+    print("odd")
+if num2%2 == 0:
+    print("even")
+elif num2%2 != 0:
+    print("odd")
+if num3%2 == 0:
+    print("even")
+elif num3%2 != 0:
+    print("odd")
