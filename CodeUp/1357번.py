c6dc782bc22b68fc07e143d3ed135bc4b7655937
Inho <stonkick@naver.com>
2021-06-06 15:17:59 +0900

    Create 1357번.py

---
+++ CodeUp/1357번.py
@@ -0,0 +1,11 @@
+num = int(input())
+
+for i in range(0, num):
+  for j in range(0, i+1):
+    print('*', end='')
+  print()
+
+for i in range(num-1, 0, -1):
+  for j in range(0, i):
+    print('*', end='')
+  print()
