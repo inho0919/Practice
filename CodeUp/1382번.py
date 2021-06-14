a104736698e80a54d643c976e6dc13174eab36cb
Inho <stonkick@naver.com>
2021-06-14 10:22:26 +0900

    Create 1382번.py

---
+++ CodeUp/1382번.py
@@ -0,0 +1,5 @@
+for i in range(1,10):
+    print("2 x " + str(i)+ " = " + str(2 * i).rjust(2), end="\t")
+    print("3 x " + str(i)+ " = " + str(3 * i).rjust(2), end="\t")
+    print("4 x " + str(i)+ " = " + str(4 * i).rjust(2), end="\t")
+    print("5 x " + str(i)+ " = " + str(5 * i).rjust(2), end="\t\n")
